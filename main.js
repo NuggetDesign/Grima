867d48c1001631435e10bb35ba38094ab4e84118
NuggetDesign <nuggetssignature@gmail.com>
2022-06-13 23:53:16 +0700

    wrapper mobile

--- main.js
+++ main.js
@@ -88,16 +88,16 @@ var colors = ["white"];
 //initially colorize each box and position in a row
 gsap.set(".box", {
   backgroundColor: (i) => colors[i % colors.length],
-  x: (i) => i * 130
+  x: (i) => i * 160
 });
 
 
 gsap.to(".box", {
-  duration: 5,
+  duration: 6,
   ease: "none",
-  x: "+=500", //move each box 500px to right
+  x: "+=600", //move each box 500px to right
   modifiers: {
-    x: gsap.utils.unitize(x => parseFloat(x) % 500) //force x value to be between 0 and 500 using modulus
+    x: gsap.utils.unitize(x => parseFloat(x) % 600) //force x value to be between 0 and 500 using modulus
   },
   repeat: -1
 });
